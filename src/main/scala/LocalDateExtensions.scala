110f1d85b9ba144595d80d251613dcae87b68947
Victor Grigoriu <grigoriu@adobe.com>
2023-11-26 11:32:17 +0200

    delete SchoolHolidays.scala, move method to LocalDateExtensions

--- src/main/scala/LocalDateExtensions.scala
+++ src/main/scala/LocalDateExtensions.scala
@@ -1,6 +1,14 @@
+import Holidays.schoolHolidays
+
 import java.time.LocalDate
 
 extension (day: LocalDate) {
+  def isSchoolHoliday: Boolean = {
+    schoolHolidays.exists { case (start, end) =>
+      start.lte(day) && day.lte(end)
+    }
+  }
+
   private def lte(other: LocalDate): Boolean =
     day.isBefore(other) || day.isEqual(other)
 }
