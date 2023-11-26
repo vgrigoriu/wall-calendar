ccf428666083ed4f96bc7eb62ee2ce5f5dab45d1
Victor Grigoriu <grigoriu@adobe.com>
2023-11-26 11:28:08 +0200

    move holidays to Holidays

--- src/main/scala/Holidays.scala
+++ src/main/scala/Holidays.scala
@@ -2,6 +2,41 @@ import java.time.LocalDate
 import java.time.Month._
 
 object Holidays {
+  val holidays: Map[LocalDate, String] = Map(
+    LocalDate.parse("2023-01-01") -> "Anul nou",
+    LocalDate.parse("2023-01-02") -> "Anul nou",
+    LocalDate.parse("2023-01-24") -> "Unirea Principatelor Române",
+    LocalDate.parse("2023-04-14") -> "Vinerea Mare",
+    LocalDate.parse("2023-04-16") -> "Paștele ortodox",
+    LocalDate.parse("2023-04-17") -> "A doua zi de Paște",
+    LocalDate.parse("2023-05-01") -> "Ziua Muncii",
+    LocalDate.parse("2023-06-01") -> "Ziua Copilului",
+    LocalDate.parse("2023-06-04") -> "Rusalii",
+    LocalDate.parse("2023-06-05") -> "A doua zi de Rusalii",
+    LocalDate.parse("2023-08-15") -> "Adormirea Maicii Domnului",
+    LocalDate.parse("2023-11-30") -> "Sfântul Andrei",
+    LocalDate.parse("2023-12-01") -> "Marea Unire",
+    LocalDate.parse("2023-12-25") -> "Crăciun",
+    LocalDate.parse("2023-12-26") -> "A doua zi de Crăciun",
+    LocalDate.of(2024, JANUARY, 1) -> "Anul nou",
+    LocalDate.of(2024, JANUARY, 2) -> "Anul nou",
+    LocalDate.of(2024, JANUARY, 6) -> "Boboteaza",
+    LocalDate.of(2024, JANUARY, 7) -> "Sf. Ioan",
+    LocalDate.of(2024, JANUARY, 24) -> "Unirea mică",
+    LocalDate.of(2024, MAY, 1) -> "Ziua muncii",
+    LocalDate.of(2024, MAY, 3) -> "Vinerea Mare",
+    LocalDate.of(2024, MAY, 5) -> "Paște",
+    LocalDate.of(2024, MAY, 6) -> "Paște",
+    LocalDate.of(2024, JUNE, 1) -> "Ziua copilului",
+    LocalDate.of(2024, JUNE, 23) -> "Rusalii",
+    LocalDate.of(2024, JUNE, 24) -> "Rusalii",
+    LocalDate.of(2024, AUGUST, 15) -> "Sf. Maria",
+    LocalDate.of(2024, NOVEMBER, 30) -> "Sf. Andrei",
+    LocalDate.of(2024, DECEMBER, 1) -> "Unirea mare",
+    LocalDate.of(2024, DECEMBER, 25) -> "Crăciun",
+    LocalDate.of(2024, DECEMBER, 26) -> "Crăciun",
+  )
+
   val schoolHolidays: Seq[(LocalDate, LocalDate)] = List(
     LocalDate.of(2024, JANUARY, 1) -> LocalDate.of(2024, JANUARY, 5),
     LocalDate.of(2024, FEBRUARY, 19) -> LocalDate.of(2024, FEBRUARY, 23),
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
+import Holidays.holidays
+
 import java.time.{DayOfWeek, LocalDate, Month}
-import java.time.Month._
+import java.time.Month.*
 
 @main def hello(): Unit =
   val year = 2024
@@ -63,41 +65,6 @@ private val monthName = Map(
   OCTOBER -> "Octombrie",
   NOVEMBER -> "Noiembrie",
   DECEMBER -> "Decembrie",
-)
-
-private val holidays = Map(
-  LocalDate.parse("2023-01-01") -> "Anul nou",
-  LocalDate.parse("2023-01-02") -> "Anul nou",
-  LocalDate.parse("2023-01-24") -> "Unirea Principatelor Române",
-  LocalDate.parse("2023-04-14") -> "Vinerea Mare",
-  LocalDate.parse("2023-04-16") -> "Paștele ortodox",
-  LocalDate.parse("2023-04-17") -> "A doua zi de Paște",
-  LocalDate.parse("2023-05-01") -> "Ziua Muncii",
-  LocalDate.parse("2023-06-01") -> "Ziua Copilului",
-  LocalDate.parse("2023-06-04") -> "Rusalii",
-  LocalDate.parse("2023-06-05") -> "A doua zi de Rusalii",
-  LocalDate.parse("2023-08-15") -> "Adormirea Maicii Domnului",
-  LocalDate.parse("2023-11-30") -> "Sfântul Andrei",
-  LocalDate.parse("2023-12-01") -> "Marea Unire",
-  LocalDate.parse("2023-12-25") -> "Crăciun",
-  LocalDate.parse("2023-12-26") -> "A doua zi de Crăciun",
-  LocalDate.of(2024, JANUARY, 1) -> "Anul nou",
-  LocalDate.of(2024, JANUARY, 2) -> "Anul nou",
-  LocalDate.of(2024, JANUARY, 6) -> "Boboteaza",
-  LocalDate.of(2024, JANUARY, 7) -> "Sf. Ioan",
-  LocalDate.of(2024, JANUARY, 24) -> "Unirea mică",
-  LocalDate.of(2024, MAY, 1) -> "Ziua muncii",
-  LocalDate.of(2024, MAY, 3) -> "Vinerea Mare",
-  LocalDate.of(2024, MAY, 5) -> "Paște",
-  LocalDate.of(2024, MAY, 6) -> "Paște",
-  LocalDate.of(2024, JUNE, 1) -> "Ziua copilului",
-  LocalDate.of(2024, JUNE, 23) -> "Rusalii",
-  LocalDate.of(2024, JUNE, 24) -> "Rusalii",
-  LocalDate.of(2024, AUGUST, 15) -> "Sf. Maria",
-  LocalDate.of(2024, NOVEMBER, 30) -> "Sf. Andrei",
-  LocalDate.of(2024, DECEMBER, 1) -> "Unirea mare",
-  LocalDate.of(2024, DECEMBER, 25) -> "Crăciun",
-  LocalDate.of(2024, DECEMBER, 26) -> "Crăciun",
 )
 
 private val adobeHolidays = Map(
